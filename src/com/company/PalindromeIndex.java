3b0b6ef1d4d156f0f077e45d9110aff655f9e977
James Park <QuantumSpark@Jamess-MacBook-Pro.local>
2016-07-13 19:58:49 -0700

    solved

--- src/com/company/PalindromeIndex.java
+++ src/com/company/PalindromeIndex.java
@@ -4,7 +4,7 @@ package com.company; /**
 import java.io.*;
 import java.util.*;
 public class PalindromeIndex {
-
+    // credit to Avinash89 and michal_lihocky
     public static void solve() {
 
        Scanner in = new Scanner(System.in);
@@ -15,24 +15,30 @@ public class PalindromeIndex {
 
         for (int i =0 ;i < n ; i++) {
             String s = in.nextLine();
-            System.out.println(getIndex(s,0,s.length()));
+            System.out.println(getIndex(s));
         }
     }
-    
-    private static int getIndex (String s, int first, int last ) {
-        int n = last;
-        for (int i = first ; i <n/2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) {
-                StringBuilder sb1 = new StringBuilder(s);
-                sb1 = sb1.deleteCharAt(i);
-                StringBuilder sb2 = new StringBuilder(s);
-                sb2 = sb2.deleteCharAt(n - i - 1);
-                if (getIndex(sb1.toString(),0,sb1.length()) == -1) {
-                    return i;
-                }
-                else if (getIndex(sb2.toString(),0,sb2.length()) == -1) {
-                    return n-i-1;
-                }
+
+    private static int getIndex (String s) {
+        int n = s.length();
+        int i = 0, j=n-1;
+        boolean notPalindrome = false;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                notPalindrome = true;
+                break;
+            }
+            j--;i++;
+        }
+        if (notPalindrome) {
+            // Check if it's a palindrome after it removes
+            // the character from the right
+            if (s.charAt(i) == s.charAt(j-1)
+                    && s.charAt(i+1) == s.charAt(j-2)) {
+                return j;
+            }
+            else {
+                return i;
             }
         }
         return -1;
